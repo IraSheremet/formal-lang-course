59eebbcd7f3cd192fbb81dcd4038a4fa5693a87f
IraSheremet <irasheremet2002@gmail.com>
2022-09-18 19:50:06 +0300

    Task2 functions

---
+++ project/task2.py
@@ -0,0 +1,60 @@
+from pyformlang.regular_expression import *
+from pyformlang.finite_automaton import *
+import networkx as nx
+from typing import Set
+
+__all__ = ["create_minimal_dfa_for_regex", "create_nfa_for_graph"]
+
+
+def create_minimal_dfa_for_regex(regex: Regex) -> DeterministicFiniteAutomaton:
+    """Create a minimal DFA for a given regular expression.
+
+    Parameters
+    ----------
+    regex : Regex
+        The specified regular expression.
+
+    Returns
+    -------
+    dfa : DeterministicFiniteAutomaton
+        The minimal deterministic finite automaton.
+    """
+    return regex.to_epsilon_nfa().to_deterministic().minimize()
+
+
+def create_nfa_for_graph(
+    graph: nx.MultiDiGraph, start_states: Set[int] = None, final_states: Set[int] = None
+) -> NondeterministicFiniteAutomaton:
+    """Create a NFA for a given graph.
+
+    Parameters
+    ----------
+    graph : MultiDiGraph
+        The graph by which the NFA is created.
+    start_states : Set[int]
+        Set of starting states.
+    final_states : Set[int]
+        Set of final states.
+
+    Returns
+    -------
+    nfa : NondeterministicFiniteAutomaton
+        The created nondeterministic finite automaton.
+    """
+    all_states = graph.nodes
+    if start_states is None:
+        start_states = all_states
+    if final_states is None:
+        final_states = all_states
+    nfa = NondeterministicFiniteAutomaton(states=all_states)
+    for st in start_states:
+        nfa.add_start_state(State(st))
+    for st in final_states:
+        nfa.add_final_state(State(st))
+
+    for edge in graph.edges:
+        if edge(data="label"):
+            nfa.add_transition(State(edge[0]), Symbol(edge[2]), State(edge[1]))
+        else:
+            nfa.add_transition(State(edge[0]), Epsilon(), State(edge[1]))
+    return nfa
