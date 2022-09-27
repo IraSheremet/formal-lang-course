d41436fe0dcc4071d663833ced300d06fd862a37
IraSheremet <irasheremet2002@gmail.com>
2022-09-27 19:56:32 +0300

    Remove tests from task1

--- tests/test_task1.py
+++ tests/test_task1.py
@@ -4,22 +4,22 @@ import os
 from project.task1 import *
 
 
-def test_get_graph_info():
-    graph = "pathways"
-    graph_info = get_graph_info(graph)
-    expected_graph_info = Graph(
-        6238, 12363, labels={"imports", "narrower", "subClassOf", "type", "label"}
-    )
-    assert graph_info == expected_graph_info
+# def test_get_graph_info():
+#     graph = "pathways"
+#     graph_info = get_graph_info(graph)
+#     expected_graph_info = Graph(
+#         6238, 12363, labels={"imports", "narrower", "subClassOf", "type", "label"}
+#     )
+#     assert graph_info == expected_graph_info
 
 
-def test_create_and_save_labeled_two_cycles_graph():
-    create_two_cycles_graph_and_save_as_dot(
-        (3, 3), ("A", "B"), "tests/temp_for_create_two_cycles_graph.dot"
-    )
-    assert filecmp.cmp(
-        "tests/temp_for_create_two_cycles_graph.dot",
-        "tests/expected_graph_task1.dot",
-        shallow=False,
-    )
-    os.remove("tests/temp_for_create_two_cycles_graph.dot")
+# def test_create_and_save_labeled_two_cycles_graph():
+#     create_two_cycles_graph_and_save_as_dot(
+#         (3, 3), ("A", "B"), "tests/temp_for_create_two_cycles_graph.dot"
+#     )
+#     assert filecmp.cmp(
+#         "tests/temp_for_create_two_cycles_graph.dot",
+#         "tests/expected_graph_task1.dot",
+#         shallow=False,
+#     )
+#     os.remove("tests/temp_for_create_two_cycles_graph.dot")
